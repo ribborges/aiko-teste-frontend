1b10fff33b49f2625cdd8de4915f7962565f929e
Richard Borges <ribborges@outlook.com>
2025-04-04 15:59:22 -0300

    feat: integrate routing and layout with Header and Navbar components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,31 @@
+import { Header } from "@/components/Header";
+import { Navbar, NavItem } from "@/components/Navbar";
+import { GeoAltFill } from "react-bootstrap-icons";
+import { Outlet } from "react-router";
+
 export default function App() {
   return (
-    <main>
-      
-    </main>
+    <div className="flex flex-col h-screen w-screen">
+      <Header />
+      <div className="flex-1 flex md:flex-row flex-col-reverse">
+        <Navbar>
+          <NavItem to="/">
+            <GeoAltFill size={24} />
+            <span>Mapa</span>
+          </NavItem>
+          <NavItem to="/about">
+            <GeoAltFill size={24} />
+            <span>Sobre</span>
+          </NavItem>
+          <NavItem to="/settings">
+            <GeoAltFill size={24} />
+            <span>Config</span>
+          </NavItem>
+        </Navbar>
+        <main className="flex-1 flex md:m-1 p-4 overflow-auto bg-zinc-200 dark:bg-zinc-900 md:rounded-xl">
+          <Outlet />
+        </main>
+      </div>
+    </div>
   )
 }
--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,12 +1,25 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router';
+import { APIProvider } from '@vis.gl/react-google-maps';
+
+import { mapsApiKey } from '@/config/env.tsx';
 
 import App from './App.tsx';
+import Map from './pages/Map.tsx';
 
 import "@/styles/global.css";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <APIProvider apiKey={mapsApiKey}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route path="/" element={<Map />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </APIProvider>
   </StrictMode>,
 );
