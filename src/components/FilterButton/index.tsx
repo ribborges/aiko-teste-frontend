ccfbb03876d3aaba0be1996726553b6dc8de1252
Richard Borges <ribborges@outlook.com>
2025-04-05 02:38:08 -0300

    feat: add FilterButton component for customizable filtering options

---
+++ src/components/FilterButton/index.tsx
@@ -0,0 +1,30 @@
+import { ReactNode } from "react";
+import clsx from "clsx";
+
+interface FilterButtonProps {
+    onClick: () => void,
+    isSelected: boolean,
+    children: ReactNode
+}
+
+function FilterButton(props: FilterButtonProps) {
+    return (
+        <button
+            className={clsx(
+                `
+                p-2
+                text-zinc-200
+                border rounded-full
+                hover:opacity-50
+                transition duration-200
+                `,
+                props.isSelected ? "bg-blue-800/50 border-blue-800" : "bg-transparent border-zinc-300 dark:border-zinc-700"
+            )}
+            onClick={props.onClick}
+        >
+            {props.children}
+        </button>
+    );
+}
+
+export { FilterButton };
