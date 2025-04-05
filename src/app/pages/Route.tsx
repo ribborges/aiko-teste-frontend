7df1cf9287ab63c22daaaede2932c6817810681c
Richard Borges <ribborges@outlook.com>
2025-04-05 02:38:14 -0300

    feat: integrate FilterButton and RouteMap components into RoutePage

--- src/app/pages/Route.tsx
+++ src/app/pages/Route.tsx
@@ -1,7 +1,31 @@
+import { FilterButton } from "@/components/FilterButton";
+import RouteMap from "@/components/Map/RouteMap";
+import { useEquipmentStore, useFilterStore } from "@/lib/store";
+
 export default function RoutePage() {
+    const { equipmentId, setEquipmentId } = useFilterStore();
+
+    const { equipments } = useEquipmentStore();
+
     return (
         <div className="flex-1 flex flex-col gap-4 overflow-auto">
             <h1 className="text-2xl font-bold">Trajeto</h1>
+            <div className="flex flex-wrap gap-1">
+                {
+                    equipments.map((equipment) => (
+                        <FilterButton
+                            key={equipment.id}
+                            isSelected={equipmentId === equipment.id}
+                            onClick={() => setEquipmentId(equipment.id)}
+                        >
+                            {equipment.name}
+                        </FilterButton>
+                    ))
+                }
+            </div>
+            <div className="flex-1 min-h-48 md:min-h-64 lg:min-h-96 max-w-screen overflow-hidden rounded-2xl text-black">
+                <RouteMap />
+            </div>
         </div>
     );
 }
