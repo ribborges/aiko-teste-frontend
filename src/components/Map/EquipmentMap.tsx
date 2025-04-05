2a8a7498ddbf5a62ee2a8466e29fa4331ea56a1e
Richard Borges <ribborges@outlook.com>
2025-04-05 00:10:52 -0300

    feat: integrate useFilterStore into EquipmentMap for enhanced equipment selection

--- src/components/Map/EquipmentMap.tsx
+++ src/components/Map/EquipmentMap.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Map } from "@vis.gl/react-google-maps";
 
 import { mapId } from "@/config/env";
-import { useEquipmentStore } from "@/lib/store";
+import { useEquipmentStore, useFilterStore } from "@/lib/store";
 
 import EquipmentMarker from "./EquipmentMarker";
 import EquipmentPopup from "./EquipmentPopup";
@@ -14,6 +14,14 @@ export default function EquipmentMap() {
     const position = { lat: -19.126536, lng: -45.947756 };
 
     const { equipments, positionHistory, models } = useEquipmentStore();
+    const { setEquipmentId } = useFilterStore();
+
+    const setSelected = (equipmentId: string | undefined) => {
+        setSelectedEquipment(equipmentId);
+        setEquipmentId(equipmentId);
+        if (equipmentId) setIsOpen(true);
+        else setIsOpen(false);
+    }
 
     return (
         <Map
@@ -27,27 +35,22 @@ export default function EquipmentMap() {
                 const convertedPosition = { lat: lastPosition.lat, lng: lastPosition.lon };
                 const equipment = equipments.find((equipments) => equipments.id === position.equipmentId);
                 const modelId = models.find((model) => model.id === equipment?.equipmentModelId)?.id;
-                
+
                 return (
                     <EquipmentMarker
                         key={index}
                         position={convertedPosition}
-                        onClick={() => {
-                            setSelectedEquipment(equipment?.id);
-                            setIsOpen(true);
-                        }}
+                        onClick={() => setSelected(equipment?.id)}
                         model={
                             modelId === "a4b0c114-acd8-4151-9449-7d12ab9bf40f" ? "harvester" :
-                            modelId === "9c3d009e-0d42-4a6e-9036-193e9bca3199" ? "tractor" :
-                            modelId === "a3540227-2f0e-4362-9517-92f41dabbfdf" ? "truck" : undefined
+                                modelId === "9c3d009e-0d42-4a6e-9036-193e9bca3199" ? "tractor" :
+                                    modelId === "a3540227-2f0e-4362-9517-92f41dabbfdf" ? "truck" : undefined
                         }
                     />
                 );
             })}
 
-            <EquipmentPopup isOpen={isOpen} onCloseClick={() => setIsOpen(false)} equipmentId={selectedEquipment}>
-                
-            </EquipmentPopup>
+            <EquipmentPopup isOpen={isOpen} onCloseClick={() => setSelected(undefined)} equipmentId={selectedEquipment} />
         </Map>
     );
 }
