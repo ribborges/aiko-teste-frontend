a6ef964bc5d9f9d312be84ed265009772b738c30
Richard Borges <ribborges@outlook.com>
2025-04-05 02:57:40 -0300

    feat: extend useFilterStore to include stateId and modelId with corresponding setters

--- src/lib/store/useFilterStore.ts
+++ src/lib/store/useFilterStore.ts
@@ -2,15 +2,23 @@ import { create } from "zustand";
 
 type state = {
     equipmentId: string | undefined;
+    stateId: string | undefined;
+    modelId: string | undefined;
 };
 
 type action = {
     setEquipmentId: (equipmentId: string | undefined) => void;
+    setStateId: (stateId: string | undefined) => void;
+    setModelId: (modelId: string | undefined) => void;
 };
 
 const useFilterStore = create<state & action>((set) => ({
     equipmentId: undefined,
+    stateId: undefined,
+    modelId: undefined,
     setEquipmentId: (equipmentId: string | undefined) => set({ equipmentId }),
+    setStateId: (stateId: string | undefined) => set({ stateId }),
+    setModelId: (modelId: string | undefined) => set({ modelId })
 }));
 
 export default useFilterStore;
