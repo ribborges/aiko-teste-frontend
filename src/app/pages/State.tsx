b1b058d25757428cbd84723491c2e630d3692be7
Richard Borges <ribborges@outlook.com>
2025-04-05 02:37:40 -0300

    fix: reverse state history array for correct chronological order

--- src/app/pages/State.tsx
+++ src/app/pages/State.tsx
@@ -43,7 +43,7 @@ export default function StatePage() {
                         second: "2-digit",
                     })
                 };
-            }) || []
+            }).reverse() || []
         )
     }, [selectedStateHistory, states, equipments]);
 
